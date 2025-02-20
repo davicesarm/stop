c61d5d225fc8eb88ec2fd136ff09f561e35f73fa
Davi César <davicesarmorais@gmail.com>
2025-02-20 18:41:45 -0300

    refactor: remove-asyncio

--- client/client.py
+++ client/client.py
@@ -1,48 +1,45 @@
-import asyncio
+import threading
 import socket
 
-class AsyncClient:
-  def __init__(self, host='127.0.0.1', port=8888):
-    self.host = host
-    self.port = port
-    self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.client_sock.setblocking(False)
-
-  async def start(self):
-    """Conecta-se ao servidor e envia uma mensagem."""
-    """ loop = asyncio.get_running_loop()
-    await loop.sock_connect(self.client_sock, (self.host, self.port))
-
-    message = "Olá, servidor!"
-    print(f"Enviando: {message}")
-    await loop.sock_sendall(self.client_sock, message.encode())
-
-    data = await loop.sock_recv(self.client_sock, 1024)
-    print("Recebido:", data.decode())
-
-    self.client_sock.close() """
-    
-    try:
-      while True:
-        loop = asyncio.get_running_loop()
-        await loop.sock_connect(self.client_sock, (self.host, self.port))
-        
-        send_data = input('>')            
-        
-        while send_data.lower() != 'exit':
-          await loop.sock_sendall(self.client_sock, send_data.encode())
-          data = await loop.sock_recv(self.client_sock, 1024)
-          print("Recebido:", data.decode())
-          send_data = input('>')
-    except Exception as e:
-      print('Erro:', e)
-    finally:
-      self.client_sock.close()
-        
+class Client:
+    def __init__(self, host='127.0.0.1', port=8888):
+        self.__host = host
+        self.__port = port
+        self.__client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          
+          
+    def receive_messages(self):
+        while True:
+            try:
+                msg = self.__client_sock.recv(1024).decode()
+                if msg.upper() == 'ENDC' or not msg:
+                    break
+                print(f"Servidor: {msg}")
+            except (ConnectionResetError, ConnectionAbortedError):
+                break
             
-          
-          
+        print("Encerrando conexão...")
+        self.__client_sock.close()
+        
+
+    def send_messages(self):
+        while True:
+            msg = input("> ")
+            if msg.lower() in ['exit', 'quit', 'sair']:
+                break
+            try: 
+                self.__client_sock.sendall(msg.encode()) 
+            except:
+                break
+
+    def connect_client(self):
+        self.__client_sock.connect((self.__host, self.__port))
+        threading.Thread(target=self.receive_messages, daemon=True).start()
+        self.send_messages()
+        print("Conexão encerrada.")
+        self.__client_sock.close()
+
 
 if __name__ == "__main__":
-  client = AsyncClient()
-  asyncio.run(client.start())
+    client = Client()
+    client.connect_client()
