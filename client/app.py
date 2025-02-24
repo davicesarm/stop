c11536efc33618173f881e91a6b1d02504b4810e
Arthur Araújo <arthurfelipe5567@gmail.com>
2025-02-24 20:43:39 -0300

    feat: add real communication with the server

--- client/app.py
+++ client/app.py
@@ -3,6 +3,7 @@ from textual.app import App
 
 # from textual.containers import VerticalScroll
 from client.screens import Entry, Game, Ranking
+from client.client import Client
 
 
 class Potstop(App):  # type: ignore
@@ -12,24 +13,43 @@ class Potstop(App):  # type: ignore
     POTS = ['CEP', 'MSÉ', 'Ator', 'Nome', 'Música', 'Carro', 'Comida', 'Objeto', 'Verbo', 'Utensílio de cozinha']
     SCREENS = {"entry": Entry}
     PLAYERS = ['arthur - 21', 'mari - 19', 'cclaras - 14', 'davis - 13']
+    
+    def __init__(self, server_host: str = "localhost", server_port: int = 8888):
+        super().__init__()
+        
+        self.title = 'Potstop'
+        self.__server_host = server_host
+        self.__server_port = server_port
 
     def on_mount(self) -> None:
         self.theme = "dracula"
+        self.__client_socket = Client(
+            server_host=self.__server_host,
+            server_port=self.__server_port,
+            on_message=self.handle_server_messages,
+        )
+        self.push_screen('entry', self.send_username)
         
-        def send_username_to_server(username: Optional[str]) -> None:
-            
-            #TODO: create send username to server logic here
-            
-            def send_pots_to_server(pots: Optional[list[str]]) -> None:
-                
-                self.push_screen(Ranking(game_leader=True, players=self.PLAYERS))
+    def send_username(self, username: Optional[str]) -> None:           
+        assert username is not None
 
-            assert username is not None
-            self.sub_title = username
-            self.push_screen(Game(pots=self.POTS), send_pots_to_server)
-            self.notify(f"{username} entrou no jogo!")
-
-        self.push_screen('entry', send_username_to_server)
+        response = self.__client_socket.send_username_to_server(username)
+        self.sub_title = username
+        
+        if not response[0]:
+            self.push_screen(Game(pots=self.POTS), self.send_pots)
+            self.notify(f"{username} joined the game!")
+        else:
+            self.push_screen('entry', self.send_username)
+            self.notify(f'{response[1]}')
+            
+    def send_pots(self, pots: Optional[list[str]]) -> None:
+        #TODO: create send pots logic
+                        
+        self.push_screen(Ranking(game_leader=True, players=self.PLAYERS))
+        
+    def handle_server_messages(self, message: str):
+        self.call_from_thread(self.notify, message)
 
 if __name__ == "__main__":
     app = Potstop()
--- client/client.py
+++ client/client.py
@@ -1,45 +1,87 @@
 import threading
 import socket
+import time
+from typing import Callable, Optional
 
 class Client:
-    def __init__(self, host='127.0.0.1', port=8888):
-        self.__host = host
-        self.__port = port
+    def __init__(self, on_message: Callable[[str], None], server_host: str = 'localhost', server_port: int = 8888):
+        self.__server_host = server_host
+        self.__server_port = server_port
         self.__client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__on_message = on_message
+        self.__pending_responses: dict[str, Optional[str]] = {}
+        self.__lock = threading.Lock()
+        threading.Thread(target=self.__connect_client, daemon=True).start()
+    
+    def send_username_to_server(self, username: str) -> tuple[int, str]:
+        try:
+            message = f"JOIN\n{username}"
+            self.__pending_responses['JOIN'] = None
+            
+            self.__client_sock.sendall(message.encode())
+            
+            response = self.__retrieve_response('JOIN')
+            
+            
+            return response
+            
+        except Exception as e:
+            return (1, f'Ocorreu um erro: ({e.__str__()})! Tente novamente.')
+        
+    def __retrieve_response(self, request_method: str, timeout: int = 2):
+        started_at = time.time()
+        
+        while time.time() - started_at < timeout:
+            with self.__lock:
+                if self.__pending_responses[request_method] is not None:
+                    response = self.__pending_responses.pop('JOIN')
+                    assert response is not None
+                    return (0 if response[1] == '0' else 1, response[3:])
+            
+            time.sleep(0.2)
+
+        return (0, 'Tempo limite para resposta do servidor!')
           
-          
-    def receive_messages(self):
+    def __receive_messages(self):        
         while True:
             try:
                 msg = self.__client_sock.recv(1024).decode()
+                # self.__on_message(msg)
+                
                 if msg.upper() == 'ENDC' or not msg:
                     break
-                print(f"Servidor: {msg}")
+                
+                if msg[0].isdigit():
+                    if msg[0] == '2':
+                        with self.__lock:
+                            self.__pending_responses['JOIN'] = msg
+                
             except (ConnectionResetError, ConnectionAbortedError):
                 break
             
-        print("Encerrando conexão...")
         self.__client_sock.close()
+        self.__reconnect_client()
+
+    def __connect_client(self):
+        try:
+            self.__client_sock.connect((self.__server_host, self.__server_port))
+            threading.Thread(target=self.__receive_messages, daemon=True).start()
         
-
-    def send_message(self):
-        while True:
-            msg = input("> ")
-            if msg.lower() in ['exit', 'quit', 'sair']:
+        except Exception:
+            self.__reconnect_client()
+        
+    def __reconnect_client(self):
+        tried = False
+        
+        while True:            
+            try:
+                self.__on_message(f'{'Problemas de conexão com o servidor, t' if not tried else 'T'}entando reconectar...')
+                self.__client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.__client_sock.connect((self.__server_host, self.__server_port))
+                self.__on_message('Conexão reestabelecida!')
+                threading.Thread(target=self.__receive_messages, daemon=True).start()
+                
                 break
-            try: 
-                self.__client_sock.sendall(msg.encode()) 
-            except:
-                break
-
-    def connect_client(self):
-        self.__client_sock.connect((self.__host, self.__port))
-        threading.Thread(target=self.receive_messages, daemon=True).start()
-        self.send_message()
-        print("Conexão encerrada.")
-        self.__client_sock.close()
-
-
-if __name__ == "__main__":
-    client = Client()
-    client.connect_client()
+            except Exception:
+                time.sleep(5)
+            tried = True
