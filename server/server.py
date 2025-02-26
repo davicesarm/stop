cf424ea12a85993be184bdd52a87c8403ed9b46d
Davi César <davicesarmorais@gmail.com>
2025-02-25 22:52:35 -0300

    refactor: remove if statements

--- server/server.py
+++ server/server.py
@@ -28,7 +28,7 @@ class Server:
     def start(self):
         self.__server_sock.bind((self.__host, self.__port))
         self.__server_sock.listen(10)
-        threading.Thread(target=self.stop_server, daemon=True).start()
+        threading.Thread(target=self.__stop_server, daemon=True).start()
         
         print(f"[+] Servidor escutando em {self.__host}:{self.__port}")
         
@@ -37,18 +37,24 @@ class Server:
                 client_sock, address = self.__server_sock.accept()
                 client = Client(client_sock, address)
                 self.__clients.add(client)
-                threading.Thread(target=self.handle_client_requests, args=(client,), daemon=True).start()
+                threading.Thread(target=self.__handle_client_requests, args=(client,), daemon=True).start()
             except (OSError, KeyboardInterrupt):
                 break
     
-    def handle_client_requests(self, client: Client):
+    
+    def broadcast(self, msg: str):
+        for client in self.__clients:
+            client.socket.sendall(msg.encode())
+    
+    
+    def __handle_client_requests(self, client: Client):
         while True:
             try:
                 msg: str = client.socket.recv(1024).decode()
                 if not msg:
                     break
                 
-                response = self.handle_message(client, msg)
+                response = self.__handle_message(client, msg)
                 client.socket.sendall(f"{response}".encode())
                 if msg.startswith('QUIT'):
                     break
@@ -59,7 +65,8 @@ class Server:
         self.__clients.delete(client)
         client.socket.close()
         
-    def validate_stop(self):
+        
+    def __validate_stop(self):
         while self.__potstop.game_started:
             while not self.__potstop.stop_queue.is_empty():
                 stop: tuple[Client, dict] = self.__potstop.stop_queue.dequeue()
@@ -82,59 +89,75 @@ class Server:
         words = self.__potstop.count_words(answers)
         for name, ans in self.__potstop.answers:
             self.__potstop.compute_points(name, ans, words)
-        print(self.__potstop.get_ranking())
+        # time.sleep(0.2)
+        # self.broadcast(f"RANK\n{self.__potstop.get_ranking()}")
+        # decidindo...
 
 
-    def handle_message(self, client: Client, msg: str):
-        if msg.startswith("QUIT"):
-            self.__potstop.remove_player(client.name)
-            return "30 Left"
-        elif msg.startswith('START'):
-            if self.__potstop.game_started:
-                return "42 Impossible"
-            if self.__potstop.get_leader() != client.name:
-                return "41 Unauthorized"
-            self.__potstop.start_game()
-            threading.Thread(target=self.validate_stop, daemon=True).start()
-            return "40 Started"
-        elif msg.startswith('JOIN'):
-            try:
-                name = msg.split('\n')[1].strip()
-            except IndexError:
-                return "0 Bad Request"
-            if name in self.__potstop.get_players() or name.upper() == "SERVER":
-                return "22 Already Joined"
-
-            self.__potstop.add_player(name, 0)
-            client.name = name
-            return "20 Joined"
-        elif msg.startswith('STOP'):
-            try:
-                data = json.loads(msg.strip().split('\n')[1])
-            except json.JSONDecodeError:
-                return "0 Bad Request"
-            
-            # Adicionar verificao caso tenha tentado dar stop sem o jogo ter começado
-
-            if not self.__potstop.stopped:
-                self.__potstop.stop_queue.enqueue((client, data))
-                return "11 Verifying Stop"
-            else:
-                self.__potstop.answers.append((client.name, data))
-                return "10 Stopped"
-            
-        else:
+    def __handle_message(self, client: Client, msg: str):
+        commands = {
+            "QUIT": self.__quit,
+            "JOIN": self.__join,
+            "START": self.__pop,
+            "STOP": self.__stop    
+        }
+        
+        if msg.split("\n")[0] not in commands:
             return "0 Bad Request"
+        
+        return commands[msg.split("\n")[0]](client, msg)
+        
+        
+    def __quit(self, client, _=None): 
+        self.__potstop.remove_player(client.name)
+        return "30 Left"
+    
+    def __join(self, client, msg):
+        try:
+            name = msg.split('\n')[1].strip()
+        except IndexError:
+            return "0 Bad Request"
+        if name in self.__potstop.get_players() or name.upper() == "SERVER":
+            return "22 Already Joined"
 
+        self.__potstop.add_player(name, 0)
+        client.name = name
+        return "20 Joined"
+        
+    def __pop(self, client, _=None):
+        if self.__potstop.game_started:
+            return "42 Impossible"
+        if self.__potstop.get_leader() != client.name:
+            return "41 Unauthorized"
+        self.__potstop.start_game()
+        self.broadcast("START")
+        threading.Thread(target=self.__validate_stop, daemon=True).start()
+        return "40 Started"
+    
+    def __stop(self, client, msg):
+        try:
+            data = json.loads(msg.strip().split('\n')[1])
+        except json.JSONDecodeError:
+            return "0 Bad Request"
+        
+        # Adicionar verificao caso tenha tentado dar stop sem o jogo ter começado
 
-    def kill_clients(self):
+        if not self.__potstop.stopped:
+            self.__potstop.stop_queue.enqueue((client, data))
+            return "11 Verifying Stop"
+        else:
+            self.__potstop.answers.append((client.name, data))
+            return "10 Stopped"
+        
+        
+    def __kill_clients(self):
         if not self.__clients.isEmpty():
             for client in self.__clients:
                 client.socket.sendall("ENDC".encode())
                 client.socket.close()
 
 
-    def stop_server(self):
+    def __stop_server(self):
         while True:
             print("Digite 'q' para encerrar o servidor.")
             try:
@@ -147,11 +170,8 @@ class Server:
                 break
         
         self.__server_sock.close()
-        self.kill_clients()
+        self.__kill_clients()
 
-    def broadcast(self, msg: str):
-        for client in self.__clients:
-            client.socket.sendall(msg.encode())
 
 if __name__ == "__main__":
     Server().start()
